2b9c08f8c9967d7754bb0d5bf29c7a9d975f15e9
Cygnet <dhakeurvashi7@gmail.com>
2025-07-19 07:03:58 +0000

    in update add logic for megre

--- app/Http/Controllers/Api/ContactApiController.php
+++ app/Http/Controllers/Api/ContactApiController.php
@@ -95,6 +95,12 @@ class ContactApiController extends Controller
 
             $contact->update($data);
 
+            //check if has merge contact then update merged_contact for master record
+            if($contact->mergedContact()->exists())
+            {
+                $this->merging_logic($contact->mergedContact->contact_uuid, $contact->mergedContact->contact_child_uuid);   
+            }
+
             return response()->json(['success' => true, 'message' => 'Contact updated successfully', 'data' => new ContactResource($contact)]);
 
         }catch(Exception $e){
@@ -132,41 +138,46 @@ class ContactApiController extends Controller
             $master_id = $request->master_id;
             $secondory_contact_id = $request->secondory_contact_id;
         
-            $master_data = Contact::find($master_id);
-            $secondory_data =  Contact::find($secondory_contact_id);
-        
-            $mergedEmails = [$master_data->email,$secondory_data->email];
-            $mergedPhones = [$master_data->Phone,$secondory_data->Phone];
-
-            $master_custom_fields = json_decode($master_data->custom_field, true) ?? [];
-            $secondory_custom_fields = json_decode($secondory_data->custom_field, true) ?? [];
-
-            $merged = collect(array_merge($master_custom_fields, $secondory_custom_fields))
-                        ->unique(fn($item) => $item['name'] . '||' . $item['value']) 
-                        ->values()
-                        ->toArray();
-        
-            $mergedJson = json_encode($merged, JSON_PRETTY_PRINT);
-        
-            // Save merged result in new table
-            $merge_contact = ContactsMerge::create([
-                'contact_uuid' => $master_id,
-                'contact_child_uuid' => $secondory_contact_id,
-                'email' => implode(',', array_unique($mergedEmails)),
-                'Phone' => implode(',', array_unique($mergedPhones)),
-                'custom_field' => $mergedJson,
-            ]);
-        
-            // mark secondory as not master
-            $secondory_data->is_master = 0;
-            $secondory_data->save();
-        
+            //call function to merge
+            $this->merging_logic($master_id, $secondory_contact_id);
             return response()->json(['success' => true, 'message' => 'Contact merged successfully', 'data' => new MergeContactResource($merge_contact)]);
         }catch(Exception $e){
             Log::info("Issue while mergeing contact for {$secondory_data['email']}: " . $e->getMessage());
 
             return response()->json(['success' => false, 'message' => 'Something went wrong', 'data' => '']);
         }
+    }
+
+    protected function merging_logic($master_id, $secondory_contact_id)
+    {
+        $master_data = Contact::find($master_id);
+        $secondory_data =  Contact::find($secondory_contact_id);
+    
+        $mergedEmails = [$master_data->email,$secondory_data->email];
+        $mergedPhones = [$master_data->Phone,$secondory_data->Phone];
+
+        $master_custom_fields = json_decode($master_data->custom_field, true) ?? [];
+        $secondory_custom_fields = json_decode($secondory_data->custom_field, true) ?? [];
+
+        $merged = collect(array_merge($master_custom_fields, $secondory_custom_fields))
+                    ->unique(fn($item) => $item['name'] . '||' . $item['value']) 
+                    ->values()
+                    ->toArray();
+    
+        $mergedJson = json_encode($merged, JSON_PRETTY_PRINT);
+    
+        // Save merged result in new table
+        $merge_contact = ContactsMerge::create([
+            'contact_uuid' => $master_id,
+            'contact_child_uuid' => $secondory_contact_id,
+            'email' => implode(',', array_unique($mergedEmails)),
+            'Phone' => implode(',', array_unique($mergedPhones)),
+            'custom_field' => $mergedJson,
+        ]);
+    
+        // mark secondory as not master
+        $secondory_data->is_master = 0;
+        $secondory_data->save();
     }
 
 }
